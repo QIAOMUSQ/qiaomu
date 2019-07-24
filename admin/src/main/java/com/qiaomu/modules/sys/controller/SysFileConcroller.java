770f2faf9525bd8969166e23c85286a7f03515c9
lipinxian <1249254045@qq.com>
2019-07-24 19:38:31 +0800

    删除多余模块

--- admin/src/main/java/com/qiaomu/modules/sys/controller/SysFileConcroller.java
+++ admin/src/main/java/com/qiaomu/modules/sys/controller/SysFileConcroller.java
@@ -1,11 +1,15 @@
 package com.qiaomu.modules.sys.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.qiaomu.common.utils.BuildResponse;
 import com.qiaomu.modules.sys.entity.SysFileEntity;
 import com.qiaomu.modules.sys.service.SysFileService;
 import jodd.io.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -89,7 +93,7 @@ public class SysFileConcroller {
      * @param id
      * @throws Exception
      */
-    @RequestMapping(value = "showPicForMany")
+    //@RequestMapping(value = "showPicForMany")
     public void showPicForMany(HttpServletRequest request, HttpServletResponse response,
                                @RequestParam Long id) throws Exception {
         if (null != id && id != -1) {
@@ -146,4 +150,29 @@ public class SysFileConcroller {
         return result;
     }
 
+    @RequestMapping(value = "showPicForMany")
+    public ResponseEntity<byte[]> downFile (HttpServletRequest request, HttpServletResponse response, @RequestParam Long id){
+        try {
+            if (null != id && id != -1) {
+                SysFileEntity fileEntity =  sysFileService.selectById(id);
+                InputStream in=new FileInputStream(new File(fileEntity.getPath()));//将该文件加入到输入流之中
+                byte[] body=null;
+                body=new byte[in.available()];// 返回下一次对此输入流调用的方法可以不受阻塞地从此输入流读取（或跳过）的估计剩余字节数
+                in.read(body);//读入到输入流里面
+
+                String fileName=new String(fileEntity.getName().getBytes("gbk"),"iso8859-1");//防止中文乱码
+                HttpHeaders headers=new HttpHeaders();//设置响应头
+                headers.add("Content-Disposition", "attachment;filename="+fileName);
+                HttpStatus statusCode = HttpStatus.OK;//设置响应吗
+                return new ResponseEntity<byte[]>(body, headers, statusCode);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        HttpHeaders headers=new HttpHeaders();//设置响应头
+        headers.add("Content-Disposition", "attachment;filename=error");
+        HttpStatus statusCode = HttpStatus.NOT_FOUND;//设置响应吗
+        byte[] body= JSON.toJSONString(BuildResponse.fail()).getBytes();
+        return new ResponseEntity<byte[]>(body,headers, statusCode);
+    }
 }
